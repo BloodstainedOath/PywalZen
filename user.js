f955c457ff84f0affe220d119f22a43a38b87ce6
BloodstainedOath <rishabh56847@gmail.com>
2025-03-26 23:35:51 +0530

    Upload

---
+++ user.js
@@ -0,0 +1,39 @@
+[
+    {
+        "property": "uc.pywalzen.darkness",
+        "label": "Darkness level",
+        "type": "dropdown",
+        "defaultValue": "default",
+        "options": [
+            {"value": "lighter", "label": "Lighter"},
+            {"value": "light", "label": "Light"},
+            {"value": "default", "label": "Default"},
+            {"value": "dark", "label": "Dark"},
+            {"value": "darker", "label": "Darker"},
+            {"value": "yet-darker", "label": "Yet darker"},
+            {"value": "pitch-black", "label": "Pitch black"}
+        ],
+        "disabledOn": []
+    },
+    {
+        "property": "uc.pywalzen.transition_speed",
+        "label": "Theme transition speed",
+        "type": "dropdown",
+        "defaultValue": "normal",
+        "options": [
+            {"value": "none", "label": "None"},
+            {"value": "fast", "label": "Fast (0.15s)"},
+            {"value": "normal", "label": "Normal (0.3s)"},
+            {"value": "slow", "label": "Slow (0.5s)"}
+        ],
+        "disabledOn": []
+    },
+    {
+        "property": "uc.pywalzen.enhance_focus",
+        "label": "Enhance focus indicators",
+        "type": "checkbox",
+        "defaultValue": true,
+        "disabledOn": []
+    }
+]
+
